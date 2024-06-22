3dd4b4f793a5235deb3427a66b019684ef5e83e9
TechPandaPro <javaisntcoffee@gmail.com>
2024-06-22 00:14:28 -0700

    Experimentation

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
-from pynput import keyboard
+from pynput.keyboard import Controller, Listener
 import time
 
+keyboard = Controller()
+
+listener = None
+
 def on_press(key):
+  global listener
+
   try:
     print('alphanumeric key {0} pressed'.format(key.char))
+    listener.stop()
+    listener = None
+    print(key.char)
     keyboard.press(key.char)
-  except AttributeError:
+  except AttributeError as error:
     print('special key {0}'.format(key))
 
-def on_release(key):
-  try:
-    print('{0} released'.format(key.char))
-    # if key == keyboard.Key.esc:
-    #   print('yup')
-    #   return False
-  except AttributeError:
-    print('special key {0}'.format(key))
+def on_press_no_suppress(key):
+  if not listener:
+    create_listener()
 
-with keyboard.Listener(on_press=on_press, on_release=on_release, suppress=True) as listener:
-  listener.join()
+def create_listener():
+  global listener
+  listener = Listener(on_press=on_press)
+  listener.start()
+
+listener_no_suppress = Listener(on_press=on_press_no_suppress)
+listener_no_suppress.start()
+
+create_listener()
+
+# prevent program from exiting
+while True:
+  time.sleep(5)
