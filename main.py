247940de68396fa92099a4b8d726405ee687e5a1
TechPandaPro <javaisntcoffee@gmail.com>
2024-06-22 00:55:29 -0700

    Make pressed key random

--- main.py
+++ main.py
@@ -1,43 +1,28 @@
 from pynput.keyboard import Controller, Listener, Key
-from random import randrange
+from random import choice, randrange
 from multiprocessing import Process
 import time
 
 keyboard = Controller()
 
+keys = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
 def on_press(key):
   random = randrange(20)
   if random == 0:
-    keyboard.press('a')
+    keyboard.press(choice(keys))
   if random == 1:
     keyboard.press(Key.backspace)
-  # try:
-  #   print('alphanumeric key {0} pressed'.format(key.char))
-  #   keyboard.press(key.char)
-  # except AttributeError:
-  #   print('special key {0}'.format(key))
-
-def on_release(key):
-  # try:
-  #   print('{0} released'.format(key.char))
-  #   # if key == keyboard.Key.esc:
-  #   #   print('yup')
-  #   #   return False
-  # except AttributeError:
-  #   print('special key {0}'.format(key))
-  return
 
 def shift_loop():
   print('running')
   while True:
     time.sleep(randrange(0, 1) + 1)
     keyboard.press(Key.shift)
-    # keyboard.press('a')
     time.sleep(1)
-    # keyboard.release('a')
 
 def run_listener():
-  with Listener(on_press=on_press, on_release=on_release) as listener:
+  with Listener(on_press=on_press) as listener:
     listener.join()
 
 if __name__ == '__main__':
