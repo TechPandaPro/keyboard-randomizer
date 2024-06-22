09aad637b71abd277b8ea0909d7f9d09e7237e5b
TechPandaPro <javaisntcoffee@gmail.com>
2024-06-22 00:47:50 -0700

    Add random keypresses

--- main.py
+++ main.py
@@ -1,36 +1,45 @@
-from pynput.keyboard import Controller, Listener
+from pynput.keyboard import Controller, Listener, Key
+from random import randrange
+from multiprocessing import Process
 import time
 
 keyboard = Controller()
 
-listener = None
-
 def on_press(key):
-  global listener
+  random = randrange(20)
+  if random == 0:
+    keyboard.press('a')
+  if random == 1:
+    keyboard.press(Key.backspace)
+  # try:
+  #   print('alphanumeric key {0} pressed'.format(key.char))
+  #   keyboard.press(key.char)
+  # except AttributeError:
+  #   print('special key {0}'.format(key))
 
-  try:
-    print('alphanumeric key {0} pressed'.format(key.char))
-    listener.stop()
-    listener = None
-    print(key.char)
-    keyboard.press(key.char)
-  except AttributeError as error:
-    print('special key {0}'.format(key))
+def on_release(key):
+  # try:
+  #   print('{0} released'.format(key.char))
+  #   # if key == keyboard.Key.esc:
+  #   #   print('yup')
+  #   #   return False
+  # except AttributeError:
+  #   print('special key {0}'.format(key))
+  return
 
-def on_press_no_suppress(key):
-  if not listener:
-    create_listener()
+def shift_loop():
+  print('running')
+  while True:
+    time.sleep(randrange(0, 1) + 1)
+    keyboard.press(Key.shift)
+    # keyboard.press('a')
+    time.sleep(1)
+    # keyboard.release('a')
 
-def create_listener():
-  global listener
-  listener = Listener(on_press=on_press)
-  listener.start()
+def run_listener():
+  with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
 
-listener_no_suppress = Listener(on_press=on_press_no_suppress)
-listener_no_suppress.start()
-
-create_listener()
-
-# prevent program from exiting
-while True:
-  time.sleep(5)
+if __name__ == '__main__':
+  p1 = Process(target=shift_loop).start()
+  p2 = Process(target=run_listener).start()
